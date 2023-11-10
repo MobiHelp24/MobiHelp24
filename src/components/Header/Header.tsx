25c64cb0f948f7dd1431efa7dddc12028069d27d
Maxoverking <mobihelpivan@gmail.com>
2023-11-11 00:01:13 +0100

    add container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,27 +7,32 @@ import { PiInstagramLogoThin, PiTelegramLogoThin } from "react-icons/pi";
 const Header: FC = () => {
   return (
     <header className={css.header}>
-      <a>Logo</a>
-      <div>
-        <TfiTime size="1.5rem" />
-        <span>ПН-ПТ 8:00 - 17:00</span>
-      </div>
-      <div className={css.contacts}>
-        <span>
-          <CiLocationOn size="1.9rem" />
-          м.Павлоград вул. Соборна 93/3
-        </span>
-        Телефон
-        <a href="tel:+380958269769">+38 (095) 826-97-69</a>
-        <a href="tel:+380996660769">+38 (099) 666-07-69</a>
-      </div>
-      <div className={css.social_media}>
-        <a href="#">
-          <PiInstagramLogoThin size="3rem" />
-        </a>
-        <a href="#">
-          <PiTelegramLogoThin size="3rem" />
-        </a>
+      <div className={`${css.container_header} container`}>
+        <a>Logo</a>
+        <div className={css.work_time}>
+          <TfiTime size="1.35rem" />
+          <span>ПН-ПТ 8:00 - 17:00</span>
+        </div>
+        <div className={css.contacts}>
+          <span className={css.address}>
+            <CiLocationOn size="1.9rem" />
+            <span>м.Павлоград вул.Соборна 93/3</span>
+          </span>
+        </div>
+        <div className={css.phone}>
+          <span>Телефон</span>
+
+          <a href="tel:+380958269769">+38 (095) 826-97-69</a>
+          <a href="tel:+380996660769">+38 (099) 666-07-69</a>
+        </div>
+        <div className={css.social_media}>
+          <a href="#">
+            <PiInstagramLogoThin size="3rem" />
+          </a>
+          <a href="#">
+            <PiTelegramLogoThin size="3rem" />
+          </a>
+        </div>
       </div>
     </header>
   );
