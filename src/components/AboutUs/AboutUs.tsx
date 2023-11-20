8fc6375b84e9a4a49ed04e3681d8ba88a220efb2
Maxoverking <avemax7@gmail.com>
2023-11-20 15:36:47 +0100

    about footer

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import css from "./AboutUs.module.css";
-// import phones from "/phones.png";
+import phones from "/phone1.png";
 import { MdOutlinePhonelinkSetup } from "react-icons/md";
 import { GiButtonFinger, GiCharging } from "react-icons/gi";
 import { IoBatteryCharging, IoWaterOutline } from "react-icons/io5";
@@ -12,42 +12,42 @@ const whatWeDo = [
     title: "Заміна екрана",
     description:
       "Тріснуте скло/тачскрін, потемніння, сенсорний екран не реагує, знебарвлення екрана",
-    icon: <MdOutlinePhonelinkSetup size="1.5rem" color="red" />,
+    icon: <MdOutlinePhonelinkSetup size="2rem" color="red" />,
   },
   {
     id: 2,
     title: "Ремонт кнопок",
     description:
       'Кнопка блокування не реагує, кнопки збільшення і зменшення гучності або кнопка "Додому" не працюють правильно',
-    icon: <GiButtonFinger size="1.5rem" color="red" />,
+    icon: <GiButtonFinger size="2rem" color="red" />,
   },
   {
     id: 3,
     title: "Заміна батареї",
     description:
       "Ви заряджаєте свій пристрій частіше, ніж зазвичай, а батарея сідає швидше і швидше.",
-    icon: <IoBatteryCharging size="1.5rem" color="red" />,
+    icon: <IoBatteryCharging size="2rem" color="red" />,
   },
   {
     id: 4,
     title: "Ремонт зарядного порту",
     description:
       "Наприклад, ви підключили до свого телефону зарядний пристрій, але смартфон не заряджається.",
-    icon: <GiCharging size="1.5rem" color="red" />,
+    icon: <GiCharging size="2rem" color="red" />,
   },
   {
     id: 5,
     title: "Ремонт динамика/микрофона",
     description:
       "Ніхто не чує вас по телефону? Або ви не чуєте співрозмовника? Глухий звук, музика не грає належним чином.",
-    icon: <IoMdMic size="1.5rem" color="red" />,
+    icon: <IoMdMic size="2rem" color="red" />,
   },
   {
     id: 6,
     title: "Залиті телефони",
     description:
       "Ми можемо оживити телефон, який піддавався впливу вологи, або був залитий якоюсь рідиною",
-    icon: <IoWaterOutline size="1.5rem" color="red" />,
+    icon: <IoWaterOutline size="2rem" color="red" />,
   },
 ];
 
@@ -55,39 +55,45 @@ const AboutUs: FC = () => {
   return (
     <div className={css.about}>
       <h2 className={css.about_main_title}>Чим ми займаємося</h2>
-      <ul>
-        {whatWeDo.map(({ title, description, icon, id }) =>
-          id < 4 ? (
-            <li key={id} className={css.item_li}>
-              <div className={css.list_div}>
-                <span>{title}</span>
-                {icon}
-              </div>
-              <p className={css.description}>{description}</p>
-            </li>
-          ) : (
-            ""
-          )
-        )}
-      </ul>
-      {/* <div>
-        <img src={phones} alt="phones" />
-      </div> */}
-      <ul>
-        {whatWeDo.map(({ title, description, icon, id }) =>
-          id > 3 ? (
-            <li key={id} className={css.item_li}>
-              <div className={css.list_div2}>
-                <span>{title}</span>
-                {icon}
-              </div>
-              <p className={css.description2}>{description}</p>
-            </li>
-          ) : (
-            ""
-          )
-        )}
-      </ul>
+      <div className={css.about_container}>
+        <ul className={css.list_container}>
+          {whatWeDo.map(({ title, description, icon, id }) =>
+            id < 4 ? (
+              <li key={id} className={css.item_li}>
+                <div className={css.list_div}>
+                  <strong>
+                    <span className={css.title}>{title}</span>
+                  </strong>
+                  {icon}
+                </div>
+                <p className={css.description}>{description}</p>
+              </li>
+            ) : (
+              ""
+            )
+          )}
+        </ul>
+        <div className={css.phone_container}>
+          <img className={css.phone} src={phones} alt="phones" />
+        </div>
+        <ul className={css.list_container2}>
+          {whatWeDo.map(({ title, description, icon, id }) =>
+            id > 3 ? (
+              <li key={id} className={css.item_li}>
+                <div className={css.list_div2}>
+                  <strong>
+                    <span className={css.title}>{title}</span>
+                  </strong>
+                  {icon}
+                </div>
+                <p className={css.description2}>{description}</p>
+              </li>
+            ) : (
+              ""
+            )
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -47,48 +47,54 @@ const Feedback: FC = () => {
 
   return (
     <>
-      <div className={css.feedbackHeader}>
-        <img
-          className={css.feedbackHeaderImg}
-          src={feedbackImg}
-          alt="feedback"
-        />
-        <h2 className={css.feedbackHeaderText}>Відгуки</h2>
-        {reviews?.length > 0 && (
-          <Swiper
-            spaceBetween={30}
-            effect={"slide"}
-            navigation={true}
-            autoplay={{
-              delay: 7000,
-              disableOnInteraction: false,
-              pauseOnMouseEnter: false,
-            }}
-            modules={[EffectCoverflow, Navigation, Pagination, Autoplay]}
-            style={{
-              width: "90%",
-              height: "auto",
-            }}
-            className="mySwiper"
-          >
-            {reviews.map((el) => (
-              <SwiperSlide
-                key={el.id}
-                style={{ opacity: 1, pointerEvents: "none" }}
-              >
-                <div className={css.reviewsContainer}>
-                  <p className={css.reviewName}>{el.item.name}</p>
-                  <Rating name="read-only" value={el.item.rating} readOnly />
-                  <div className={css.reviewBody}>{el.item.review}</div>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
-        <button className={css.reviewButton} onClick={() => modalShow()}>
-          Залишити відгук{" "}
-        </button>
-        {isModalShow && <Modal setModalHide={setIsModalShow} />}
+      <div className={css.feedback_background}>
+        <div className={`${css.feedbackHeader} container`}>
+          <img
+            className={css.feedbackHeaderImg}
+            src={feedbackImg}
+            alt="feedback"
+          />
+          <h2 className={css.feedbackHeaderText}>Відгуки</h2>
+          {reviews?.length > 0 && (
+            <Swiper
+              spaceBetween={30}
+              effect={"slide"}
+              navigation={true}
+              autoplay={{
+                delay: 7000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: false,
+              }}
+              modules={[EffectCoverflow, Navigation, Pagination, Autoplay]}
+              style={{
+                width: "90%",
+                height: "auto",
+              }}
+              className="mySwiper"
+            >
+              {reviews.map((el) => (
+                <SwiperSlide
+                  key={el.id}
+                  style={{ opacity: 1, pointerEvents: "none" }}
+                >
+                  <div className={css.reviewsContainer}>
+                    <p className={css.reviewName}>
+                      {" "}
+                      <strong>{el.item.name}</strong>
+                    </p>
+
+                    <Rating name="read-only" value={el.item.rating} readOnly />
+                    <div className={css.reviewBody}>{el.item.review}</div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+          <button className={css.reviewButton} onClick={() => modalShow()}>
+            Залишити відгук{" "}
+          </button>
+          {isModalShow && <Modal setModalHide={setIsModalShow} />}
+        </div>
       </div>
     </>
   );
